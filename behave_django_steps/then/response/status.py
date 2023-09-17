a4f98f2e9a2051304b10b82ec1a9764f915d4109
BillSchumacher <34168009+BillSchumacher@users.noreply.github.com>
2023-09-17 12:57:29 -0500

    If the response is not a JSONResponse convert the data from the returned content.

--- behave_django_steps/then/response/status.py
+++ behave_django_steps/then/response/status.py
@@ -1,4 +1,7 @@
+import json
+
 from behave import then
+from rest_framework.response import Response
 
 
 @then("a response is available")
@@ -11,5 +14,8 @@ def response_status_code_is(context, status_code):
     context.execute_steps(f"Then a response is available")
     print(context.response)
     if hasattr(context.response, "data"):
+        if isinstance(context.response, Response):
+            context.response.data = json.loads(context.response.content)
         print(context.response.data)
+        print(context.response.content)
     context.test.assertEqual(context.response.status_code, int(status_code))
