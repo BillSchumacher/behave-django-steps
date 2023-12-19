0d3c522cd02bbc31580b332d1843f240ca5dedfd
BillSchumacher <34168009+BillSchumacher@users.noreply.github.com>
2023-12-19 15:09:00 -0600

    Fixed an issue where it's not possible to send custom headers.

--- behave_django_steps/when/request/execute.py
+++ behave_django_steps/when/request/execute.py
@@ -18,14 +18,18 @@ def api_client_is_available(context):
 @when('I make a "{method}" request to "{url}"')
 def make_request(context, method, url):
     """Make a request."""
-    context.response = getattr(context.test.client, method.lower())(url)
+    context.response = getattr(context.test.client, method.lower())(
+        url, headers=context.headers if hasattr(context, "headers") else None
+    )
 
 
 @when('I make a "{method}" request to "{url}" with data')
 def make_request_with_data(context, method, url):
     """Make a request with data."""
     context.response = getattr(context.test.client, method.lower())(
-        url, data=context.request_data
+        url,
+        data=context.request_data,
+        headers=context.headers if hasattr(context, "headers") else None,
     )
 
 
@@ -34,7 +38,9 @@ def make_api_request(context, method, url):
     """Make an API request."""
     context.execute_steps("Given api client is available")
     context.response = getattr(context.test.api_client, method.lower())(
-        url, format="json"
+        url,
+        format="json",
+        headers=context.headers if hasattr(context, "headers") else None,
     )
 
 
@@ -43,5 +49,8 @@ def make_api_request_with_data(context, method, url):
     """Make an API request with data."""
     context.execute_steps("Given api client is available")
     context.response = getattr(context.test.api_client, method.lower())(
-        url, data=context.request_data, format="json"
+        url,
+        data=context.request_data,
+        format="json",
+        headers=context.headers if hasattr(context, "headers") else None,
     )
